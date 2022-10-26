12329194033e149b47ca560811e658ef83bf5b10
marcelo.brito <marcelo.brito@g.globo>
2022-10-25 22:39:15 -0300

    feat(nav): add new

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <nav>
-    <a href="/">Home</a>
-    <a href="/maps">Maps</a>
+    <a href="/27">27+</a>
+    <a href="/casais">Casais</a>
+    <a href="/next">Next</a>
+    <a href="/pink">Pink</a>
+    <a href="/nexteen-meninos">Nexteen Meninos</a>
+    <a href="/nexteen-meninas">Nexteen Meninas</a>
 </nav>
 
 <slot></slot>
