6ae79dd6e57fae87e782e0996e8fc9f86b51acee
marcelo.brito <marcelo.brito@g.globo>
2022-10-26 12:15:04 -0300

    fix: file generation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
     <a href="/gc-kit/pink">Pink</a>
     <a href="/gc-kit/nexteen-meninos">Nexteen Meninos</a>
     <a href="/gc-kit/nexteen-meninas">Nexteen Meninas</a>
-    <a href="/changelog">3.1.0</a>
+    <a href="/changelog">3.1.2</a>
 </nav>
 
 <slot></slot>
