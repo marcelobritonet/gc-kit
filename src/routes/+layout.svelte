bcf1c7cce9dc1009066e9ab8b9e352418dbe5148
marcelo.brito <marcelo.brito@g.globo>
2022-11-01 21:35:28 -0300

    feat: add misto

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,11 @@
     <a href="/gc-kit/casais">Casais</a>
     <a href="/gc-kit/next">Next</a>
     <a href="/gc-kit/pink">Pink</a>
+    <a href="/gc-kit/misto">Misto</a>
     <a href="/gc-kit/nexteen-meninos">Nexteen Meninos</a>
     <a href="/gc-kit/nexteen-meninas">Nexteen Meninas</a>
-    <a href="/changelog">3.1.2</a>
 </nav>
 
 <slot></slot>
+
+<a href="/changelog">3.2.0</a>
---
+++ src/routes/gc-kit/misto/+page.svelte
@@ -0,0 +1,193 @@
+<svelte:head>
+
+</svelte:head>
+
+<style>
+    @font-face {
+        font-family: 'Consola';
+        src: url("/font/consola.ttf");
+    }
+    @font-face {
+        font-family: 'Gobold Bold';
+        src: url("/font/gobold-bold.otf");
+    }
+    @font-face {
+        font-family: 'Hanson Bold';
+        src: url("/font/hanson-bold.ttf");
+    }
+    .wrapper {
+        margin: 0;
+        padding: 0;
+        position: relative;
+    }
+    .form {
+
+    }
+    .folder {
+        position: absolute;
+        width: 576px;
+        height: 1024px;
+        margin: 0;
+        padding: 0;
+        top: 0;
+        left: 0;
+    }
+    .base{
+        max-height: 1024px;
+    }
+    .title {
+        color: black;
+        position: absolute;
+        font-size: 27px;
+        text-transform: uppercase;
+        font-family: 'Hanson Bold', sans-serif;
+        text-align: center;
+        letter-spacing: 1px;
+        top: 404px;
+        left: 35px;
+        right: 30px;
+    }
+    .date {
+        color: black;
+        position: absolute;
+        font-size: 23px;
+        text-transform: uppercase;
+        font-family: 'Gobold Bold', sans-serif;
+        text-align: center;
+        letter-spacing: -1.1px;
+        top: 504px;
+        left: 25px;
+        right: 20px;
+    }
+    .address {
+        color: black;
+        position: absolute;
+        font-family: 'Consola', sans-serif;
+        text-transform: uppercase;
+        right: 30px;
+        left: 32px;
+        text-align: center;
+        line-height: 21px;
+        font-size: 24px;
+        letter-spacing: -0.6px;
+    }
+    .address_top {
+        top: 606px;
+
+    }
+    .address_bottom {
+        top: 629px;
+    }
+    .contact {
+        color: black;
+        position: absolute;
+        font-family: 'Consola', sans-serif;
+        text-transform: uppercase;
+        text-align: center;
+        left: 52px;
+        right: 50px;
+        font-size: 22px;
+        letter-spacing: -0.4px;
+    }
+    .contact-first {
+        top: 703px;
+    }
+    .contact-second {
+        top: 728px;
+    }
+</style>
+
+<script>
+    import { onMount } from "svelte";
+    import {
+        retrieveLastData,
+        generate,
+        storageLastData,
+        capitalizeString,
+        deleteStorageDataByHash
+    } from "../+layout.js";
+
+    let lastData = [];
+
+    let prefix = "GC Next";
+    let name;
+    let date;
+    let address_top;
+    let address_bottom;
+    let contact_1;
+    let contact_2;
+
+    onMount(async () => {
+        lastData = retrieveLastData()
+    });
+
+    async function handleGenerate() {
+        const filename = `${prefix} ${capitalizeString(name)} - ${date.toUpperCase()}.jpeg`
+        const obj = {
+            prefix: prefix,
+            name: name,
+            date: date,
+            address_top: address_top,
+            address_bottom: address_bottom,
+            contact_1: contact_1,
+            contact_2: contact_2
+        }
+        generate(filename)
+        await storageLastData(obj)
+        updateLastDataList()
+    }
+
+    function handleDelete(hash) {
+        deleteStorageDataByHash(hash)
+        updateLastDataList()
+    }
+
+    function updateLastDataList() {
+        lastData = retrieveLastData()
+    }
+
+    function handleRestoreData(lastData) {
+        name = lastData.name || "nome do gc";
+        date = lastData.date || "data hora";
+        address_top = lastData.address_top || "endereço";
+        address_bottom = lastData.address_bottom || "complemento";
+        contact_1 = lastData.contact_1 || "nome telefone";
+        contact_2 = lastData.contact_2 || "nome telefone";
+    }
+</script>
+
+<div class="form">
+    <input bind:value={name} type="text" placeholder="Nome do GC">
+    <input bind:value={date} type="text" placeholder="Dia da Semana / Horário">
+    <input bind:value={address_top} type="text" placeholder="Endereço">
+    <input bind:value={address_bottom} type="text" placeholder="Complemento">
+    <input bind:value={contact_1} type="text" placeholder="Contato 1">
+    <input bind:value={contact_2} type="text" placeholder="Contato 2">
+
+    <button on:click={handleGenerate}>Baixar Arte</button>
+    <button on:click={handleDelete}>Delete</button>
+
+    <ul>
+        {#each lastData as last}
+            <li>{last.name} - {last.date}</li>
+            <li>{last.address_top} - {last.address_bottom}</li>
+            <li>{last.contact_1}</li>
+            <li>{last.contact_2}</li>
+            <button on:click|preventDefault={ () => handleDelete(last.hash) }>Delete</button>
+            <button on:click|preventDefault={ () => handleRestoreData(last) }>Restaurar</button>
+        {/each}
+    </ul>
+</div>
+
+<div class="wrapper">
+    <div class="folder" id="kit">
+        <p class="title">{name}</p>
+        <p class="date">{date}</p>
+        <p class="address address_top">{address_top}</p>
+        <p class="address address_bottom">{address_bottom}</p>
+        <p class="contact contact-first">{contact_1}</p>
+        <p class="contact contact-second">{contact_2}</p>
+        <img class="base" src="../misto/MISTO-BASE.png" alt="">
+<!--        <img class="base" src="../misto/MISTO-EXEMPLO.png" alt="">-->
+    </div>
+</div>
